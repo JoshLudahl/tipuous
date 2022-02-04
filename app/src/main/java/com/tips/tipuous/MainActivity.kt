319a056d3450381a0aec4d3cee580b67daac0754
Joshy <josh.ludahl@gmail.com>
2022-02-03 20:59:12 -0800

    Deps

--- app/src/main/java/com/tips/tipuous/MainActivity.kt
+++ app/src/main/java/com/tips/tipuous/MainActivity.kt
@@ -32,10 +32,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        try {
-            this.supportActionBar!!.hide()
-        } catch (e: NullPointerException) {
-        }
 
         binding = DataBindingUtil.setContentView<ActivityMainBinding?>(this, R.layout.activity_main).apply {
             composeView.setContent {
